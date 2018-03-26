d7c55296d3620d040a5871f32af1e666f0be0697
Yan Paulo <yanpaulo@hotmail.com>
2018-03-26 08:29:09 -0300

    Adicionar arquivos de projeto.

---
+++ VisitaCidades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisitaCidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
